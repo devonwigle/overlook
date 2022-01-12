f7cc676efd26683516f9b01f0cc52055281a4192
Devon Wigle <devonwigle@gmail.com>
2022-01-12 10:30:38 -0600

    Add imports to test files

---
+++ test/customer-test.js
@@ -0,0 +1,5 @@
+import { expect } from 'chai';
+import bookingsData from './test-data/booking-data';
+import roomsData from './test-data/room-data';
+import customersData from './test-data/customer-data';
+import Customer from './src/classes/customer';
