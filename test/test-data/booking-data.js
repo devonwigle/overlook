537126abb4ddf79fc1318995ad6f98e1c25e4faa
Devon Wigle <devonwigle@gmail.com>
2022-01-13 20:51:33 -0600

    Fix indentation

--- test/test-data/booking-data.js
+++ test/test-data/booking-data.js
@@ -1,38 +1,38 @@
 const bookingsData = [  
   {
     id: "5fwrgu4i7k55hl6ty",
-      userID: 50,
-        date: "2022/01/10",
-          roomNumber: 25,
-            roomServiceCharges: []
+    userID: 50,
+    date: "2022/01/10",
+    roomNumber: 25,
+    roomServiceCharges: []
   },
   {
     id: "5fwrgu4i7k55hl6xu",
-      userID: 50,
-        date: "2022/02/25",
-          roomNumber: 19,
-            roomServiceCharges: []
+    userID: 50,
+    date: "2022/02/25",
+    roomNumber: 19,
+    roomServiceCharges: []
   },
   {
     id: "5fwrgu4i7k55hl71d",
-      userID: 50,
-        date: "2022/02/03",
-          roomNumber: 9,
-            roomServiceCharges: []
+    userID: 50,
+    date: "2022/02/03",
+    roomNumber: 9,
+    roomServiceCharges: []
   },
   {
     id: "5fwrgu4i7k55hl77h",
-      userID: 50,
-        date: "2022/01/21",
-          roomNumber: 13,
-            roomServiceCharges: []
+    userID: 50,
+    date: "2022/01/21",
+    roomNumber: 13,
+    roomServiceCharges: []
   }, 
   {
     id: "5fwrgu4i7k55hl77i",
-      userID: 1,
-        date: "2022/01/22",
-          roomNumber: 17,
-            roomServiceCharges: []
+    userID: 1,
+    date: "2022/01/22",
+    roomNumber: 17,
+    roomServiceCharges: []
   }
 ]
 
