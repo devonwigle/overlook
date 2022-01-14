a6072e2de254f07e5e1700e0e0f444961e704650
Devon Wigle <devonwigle@gmail.com>
2022-01-14 06:16:24 -0600

    Write booking constructor

--- src/classes/booking.js
+++ src/classes/booking.js
@@ -1,5 +1,10 @@
 class Booking {
-
+  constructor(bookingsData) {
+    this.id = bookingsData.id;
+    this.userID = bookingsData.userID;
+    this.date = bookingsData.date;
+    this.roomNumber = bookingsData.roomNumber;
+  }
 }
 
 export default Booking
