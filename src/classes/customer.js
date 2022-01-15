47281c76d4ad0ac8755dd9dd775bf3006e2d3a55
Devon Wigle <devonwigle@gmail.com>
2022-01-14 21:48:00 -0600

    Add user name and password to constructor

--- src/classes/customer.js
+++ src/classes/customer.js
@@ -2,7 +2,9 @@ class Customer {
   constructor(customersData) {
     this.id = customersData.id;
     this.name = customersData.name;
-    this.bookings = []
+    this.bookings = [];
+    this.userName = `customer${this.id}`;
+    this.password = 'overlook2021'
   }
 }
 
