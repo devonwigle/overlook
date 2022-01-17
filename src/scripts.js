f47c156900060224de8a5f295971cf6e8283c327
Devon Wigle <devonwigle@gmail.com>
2022-01-17 10:53:11 -0600

    Fix data accessibility

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,51 @@
 
 import {fetchAllData, fetchSingleUser, postData} from './APICalls';
 import Hotel from './classes/hotel';
+import domUpdates from './domUpdates'
 import './css/base.scss';
+import Customer from './classes/customer';
 
+// Global variables
 let hotel;
+let currentUser;
+let rooms;
 
-document.querySelector('button').addEventListener('click', fetchTheData())
 
-function fetchTheData() {
-  let roomsPromise = fetchAllData('rooms');
-  let customersPromise = fetchAllData('customers');
-  let bookingsPromise = fetchAllData('bookings');
-  
-  Promise.all([roomsPromise, customersPromise, bookingsPromise])
-    .then(data => hotel = new Hotel(data[0], data[1], data[2]))
-    .then(() => console.log(hotel))
+// Functions
+const loadPage = () => {
+  fetchTheData()
+  .then(data => {
+    hotel = new Hotel(data[0], data[1], data[2])
+    addInfo()
+    defineUser(50)
+    domUpdates.greetUser(currentUser)
+  })
 }
-console.log('This is the JavaScript entry file - your code begins here.');
+
+const addInfo = () => {
+  hotel.addRooms();
+  hotel.addBookings();
+  hotel.addCustomers();
+}
+
+const fetchTheData = () => {
+  const response = Promise.all([fetchAllData('rooms'), fetchAllData('customers'), fetchAllData('bookings')])
+  return response
+}
+
+const defineUser = (userID) => {
+  currentUser = hotel.allCustomers[userID - 1]
+  console.log('user', currentUser)
+  return currentUser
+}
+const iAmAFunction = () => {
+  console.log('hello')
+  console.log('function', hotel)
+}
+//eventlisteners
+window.addEventListener('load', () => {
+  loadPage()
+  domUpdates.greetUser(currentUser)
+})
+
+export default currentUser
