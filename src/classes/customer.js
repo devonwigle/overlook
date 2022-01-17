4e0bc18d8f6043e2061aee64d442303e4a444f20
Devon Wigle <devonwigle@gmail.com>
2022-01-17 12:09:19 -0600

    Add sum bookings method

--- src/classes/customer.js
+++ src/classes/customer.js
@@ -1,3 +1,4 @@
+import Hotel from './hotel';
 class Customer {
   constructor(customersData) {
     this.id = customersData.id;
@@ -5,6 +6,18 @@ class Customer {
     this.bookings = [];
     this.userName = `customer${this.id}`;
     this.password = 'overlook2021'
+  }
+
+  sumUserBookings(hotel) {
+    let sum = this.bookings.reduce((sum, booking) => {
+      hotel.allRooms.forEach(room => {
+        if (room.roomNumber === booking.roomNumber) {
+          sum += room.cost
+        }
+      })
+      return sum
+    }, 0)
+    return sum
   }
 }
 
