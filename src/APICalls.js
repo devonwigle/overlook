4c8cbede1377255422279e4f5877c26d4b30af1f
Devon Wigle <devonwigle@gmail.com>
2022-01-16 21:24:01 -0600

    Fix APIs

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,20 +1,27 @@
 let url = `http://localhost:3001/api/v1/`
 
 const fetchAllData = (extraBit) => {
-    return fetch(`${url}${extrabit}`)
-      .then(response => response.json())
-      .then(data => data[extraBit])
-      .catch(error => (error))
-  },
-
-  postData(newPost) {
-    return fetch(`${url}bookings`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(newPost)
-    })
+  return fetch(`${url}${extraBit}`)
     .then(response => response.json())
-  }
+    .then(data => data[extraBit])
+    .catch(error => (error))
 }
 
-export default apiCalls
+const fetchSingleUser = (idNum) => {
+  return fetch(`${url}user/${idNum}`)
+    .then(response => response.json())
+    .then(data => data[idNum])
+    .catch(error => (error))
+}
+
+const postData = (newPost) => {
+  return fetch(`${url}bookings`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(newPost)
+  })
+  .then(response => response.json())
+}
+
+
+export {fetchAllData}
