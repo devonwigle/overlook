5d4bc719a485773763ffa956207180569205abd2
Devon Wigle <devonwigle@gmail.com>
2022-01-17 14:09:07 -0600

    Add functions for greeting and displaying bookings and cost

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,32 @@
+import { fetchAllData, fetchSingleUser, postData } from './APICalls';
+import Hotel from './classes/hotel';
+import currentUser from './scripts'
+
+// Global variables
+let hotel;
+let sum;
+// let currentUser;
+
 let domUpdates = {
-  
+  greetUser(currentUser) {
+    let userBox = document.querySelector('.user-box');
+    userBox.innerText = `Hello ${currentUser.name}`;
+  },
+  displayUsersExpenses(sum) {
+    let amountSpent = document.querySelector('.amount');
+    amountSpent.innerText = `Amount Spent: $${sum}`
+  },
+  displayUserBookings(currentUser) {
+    let imageScroll = document.querySelector('.image-scroll');
+    imageScroll.innerHTML = ``
+    currentUser.bookings.forEach(booking => imageScroll.innerHTML += `
+      <div class="booking">
+        <p>Date of Stay: ${booking.date}</p>
+        <p>Room Number: ${booking.roomNumber}</p>
+        <p>Confirmation Number: ${booking.id}</p>
+      </div>`
+    )
+  }
 }
 
-export {domUpdates}
+export default domUpdates
